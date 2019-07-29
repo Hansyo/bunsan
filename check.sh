804db6e0b39e58ba276810a0f68226f29ea86f2a
Hansyo <hansyo.souti.1999@gmail.com>
2019-07-29 09:50:29 +0900

    dataのupdate

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for ((i=1; i <= 128;i++));do
-	for ((j=1; j <= 10;j++));do
+	for ((j=1; j <= 5;j++));do
 		./$1 $i >> result/result_$1_$i.txt
 		tail -n 1 result/result_$1_$i.txt
 	done
--- result/calc_result.sh
+++ result/calc_result.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 for((i=1;i <= 128;i++));do
 RESULT="0"
-F_NAME=`echo "result_mat_mul_base.phi_$i.txt"`
+F_NAME=`echo "result_mat_mul_base_O3.phi_$i.txt"`
 for line in `cat $F_NAME`;do
 RESULT=`echo "$RESULT + $line" | bc`
 done
 RESULT=`echo "scale=6; $RESULT / 10" | bc`
 echo "$i $RESULT"
-echo "$i $RESULT" >> result_all.txt
+echo "$i $RESULT" >> result_all_mat_mul_base_O3.phi.txt
 done
